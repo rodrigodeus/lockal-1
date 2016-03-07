2453d399a8776232d31d1acabbf57d7efd7527f2
rodrigo-martins <rodrigo.martins@lab212.com.br>
2016-03-07 01:00:10 -0300

    contratos por cliente, contratos e clintes por parceiros, troca do login usuarios por parceiros, administracao de hierarquia admin/parceiro

---
+++ backend/delete_contrato.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rodrigo.martins
+ * Date: 22/01/2016
+ * Time: 16:57
+ */
+
+include_once "first_all.php";
+
+$bd = new BD();
+
+    $table = "clientes";
+    $dados['ativo'] =  "\"false\"";
+    $where = "codigo=".$_GET['codigo'];
+    $bd->update($table,$dados,$where);
+    $bd->record_log("log_login",'delete_cliente',$_GET['codigo']);
+
+header('Location: ../pages/clientes.php');
